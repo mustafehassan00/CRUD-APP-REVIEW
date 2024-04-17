b8b21396be4d0c1dff2b16d90b74fdc54ccd8863
Mustafe Hassan <mustafeh123@gmail.com>
2024-04-17 14:06:24 -0500

    updated the server side to match the name of the db

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const thingsRouter = require('./routes/things.router.js');
+const usRouter = require('./routes/us.router.js');
 
 const PORT = 5001;
 
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json())
 app.use(express.static('server/public'));
-app.use('/things', thingsRouter);
+app.use('/us', usRouter);
 
 app.listen(PORT, () => {
   console.log(`server is up and running at http://localhost:${PORT}`)
